1063d8aba31ef13b39386bef5ebce6af4de9930d
dong shuang <dsfat.lover@gmail.com>
2020-12-14 13:10:31 +0800

    Unique Binary Search TreesII

---
+++ algorithms/java/src/uniqueBinarySearchTreesII/UniqueBinarySearchTreesII.java
@@ -0,0 +1,53 @@
+// Source: https://leetcode.com/problems/unique-binary-search-trees-ii/
+
+package uniqueBinarySearchTreesII;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class UniqueBinarySearchTreesII {
+
+    public static List<TreeNode> generateTrees(int n) {
+        List<TreeNode>[] result = new List[n + 1];
+        result[0] = new ArrayList<TreeNode>();
+        if (n == 0) {
+            return result[0];
+        }
+
+        result[0].add(null);
+        for (int len = 1; len <= n; len++) {
+            result[len] = new ArrayList<TreeNode>();
+            for (int j = 0; j < len; j++) {
+                for (TreeNode nodeL : result[j]) {
+                    for (TreeNode nodeR : result[len - j - 1]) {
+                        TreeNode node = new TreeNode(j + 1);
+                        node.left = nodeL;
+                        node.right = clone(nodeR, j + 1);
+                        result[len].add(node);
+                    }
+                }
+            }
+        }
+        return result[n];
+    }
+
+    private static TreeNode clone(TreeNode n, int offset) {
+        if (n == null) {
+            return null;
+        }
+        TreeNode node = new TreeNode(n.val + offset);
+        node.left = clone(n.left, offset);
+        node.right = clone(n.right, offset);
+        return node;
+    }
+
+    public static void main(String[] args) {
+
+    }
+}
+
+/*
+1. 可以通过偏移量解决右边部分的形状
+2. list 数组概念
+3. result[0]  巧妙解决了 Null 值问题
+ */
